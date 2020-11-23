be36e8bbe5df1edc92debef128349b1befa7966d
デンパ時計 <jsoldenpa@yahoo.co.jp>
2020-11-23 21:13:45 +0900

    プロジェクト ファイルを追加します。

---
+++ VMCSetting/Views/VMCSettingViewController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using BeatSaberMarkupLanguage;
+using BeatSaberMarkupLanguage.Attributes;
+using BeatSaberMarkupLanguage.Components;
+using BeatSaberMarkupLanguage.ViewControllers;
+
+
+namespace VMCSetting.Views
+{
+    internal class VMCSettingViewController : PersistentSingleton<VMCSettingViewController>
+    {
+        
+
+        // For this method of setting the ResourceName, this class must be the first class in the file.
+        public string ResourceName => string.Join(".", GetType().Namespace, GetType().Name);
+
+        [UIAction("calibrate")]
+        void ClickedCalibrate()
+        {
+
+        }
+
+        void RequestCalibrate()
+        {
+
+        }
+
+        void ExecuteCalibrate()
+        {
+
+        }
+    }
+}
